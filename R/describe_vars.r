18bebefa9923d9737428d6847a2c1a4a8af26d4f
Philipp Masur <philippmasur@Philipps-MacBook-Pro.local>
2019-02-02 21:08:36 -0500

    fit_table.r added

---
+++ R/describe_vars.r
@@ -0,0 +1,43 @@
+#' Customized version of the function describe (psych)
+#' 
+#' This function is essentially similar to the function \code{describe()} in the package \code{psych} (it even uses that function), but provides some additional benefits: First, it transforms the the output into a tibble, second, it reduces number of psychometrics  to the most important ones, and it allows to include item formulation drawn from an additional data frame (needs to have a variable "code" that includes similar variable codes as the original data frame and a variable named "item" that includes the item formulations).
+#' 
+#' @param data A data frame that includes that variables that should be evaluated.
+#' @param items A data frame containing the variable codes and item formulations.
+#' @param brief A logical value indicating wether all psychometrics or only the mean and the standard deviations should be printed
+#' @examples 
+#' d <- mtcars
+#' 
+#' describe_vars(mtcars)
+#' @export
+describe_vars <- function(data, items = NULL, brief = FALSE) {
+  
+  # dependencies
+  library(psych)
+  library(tidyverse)
+  
+  temp <- data %>%
+    describe %>%
+    as.data.frame %>%
+    rownames_to_column("code") 
+  
+  if (!is.null(items)) {
+    temp <- temp %>%
+      left_join(items) %>%
+      select(code, item, mean, sd, min, max, skew, kurtosis, n)
+  } else {
+    temp <- temp %>%
+      select(code, mean, sd, min, max, skew, kurtosis, n)
+  }
+  
+  if (brief == TRUE) {
+    temp %>%
+      select(code, item, mean, sd) %>%
+      as.tibble
+  } else {
+    temp %>% 
+      as.tibble
+  }
+}
+
+
---
+++ R/fit_table.r
@@ -0,0 +1,72 @@
+#' Creates a table with relevant fit indices for an SEM
+#' 
+#' Coming soon
+#' 
+#' @param object An object of class \code{lavaan} created by using \code{cfa()} or \code{sem()} from the package 'lavaan'.
+#' @param indices Vector of the indices that should be in the ouput.
+#' @param reliability Logical value indicating whether Cronbach's alpha, McDonald's omega (composite reliability), and the average variance extracted should be included-
+#' @param robust Logical value indicating whether the model was fitted with a robust estimator. If yes, scaled indices will be used. 
+#' @examples 
+#' model <- '
+#' # latent variables
+#' ind60 =~ x1 + x2 + x3
+#' dem60 =~ y1 + y2 + y3 + y4
+#' dem65 =~ y5 + y6 + y7 + y8
+#' 
+#' # regressions
+#' dem60 ~ ind60
+#' dem65 ~ ind60 + dem60
+#' 
+#' # residual covariances
+#' y1 ~~ y5
+#' y2 ~~ y4 + y6
+#' y3 ~~ y7
+#' y4 ~~ y8
+#' y6 ~~ y8
+#' '
+#' fit <- sem(model,
+#' data = PoliticalDemocracy)
+#' fit_table(fit)
+#' @export
+fit_table <- function(object,
+                      indices = c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "rmsea.ci.lower", "rmsea.ci.upper", "srmr"),
+                      reliability = TRUE,
+                      robust = FALSE) {
+  # dependencies
+  library(lavaan)
+  library(semTools)
+  library(tidyverse)
+  library(papaja)
+  
+  # function
+  if(!isTRUE(robust)){
+    fit_indices <- inspect(object, what = "fit") %>% 
+      .[indices]
+  } else {
+    indices_robust <- paste0(indices, ".scaled")
+    if(isTRUE("srmr.scaled" %in% indices_robust)) (
+      indices_robust[indices_robust == "srmr.scaled"] <- "srmr_bentler"
+    )
+    
+    fit_indices <- inspect(object, what = "fit") %>% .[indices_robust] %>% 
+      set_names(indices)
+  }
+  
+  # creating table
+  fit_indices <- fit_indices %>%
+    t %>% as.tibble %>%
+    mutate_at(vars(chisq), funs(printnum(.))) %>% 
+    mutate_at(vars(cfi, tli, rmsea, rmsea.ci.lower, rmsea.ci.lower, srmr), funs(printnum(., gt1 = F, zero = F))) %>% 
+    mutate_at(vars(pvalue), funs(printp(.)))
+  
+  if(isTRUE(reliability)) {
+  fit_indices <- fit_indices %>% 
+      mutate(alpha = semTools::reliability(object)["omega3", "total"],
+             omega = semTools::reliability(object)["alpha", "total"],
+             ave = semTools::reliability(object)["avevar", "total"]) %>%
+      mutate_at(vars(alpha, omega, ave), funs(printnum(., gt1 = F, zero = F)))
+  }
+  return(fit_indices)
+}
+
+
