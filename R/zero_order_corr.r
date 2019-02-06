1fe82abb7c9db3ec2f0b544f2848366b75d15e1f
Philipp Masur <philippmasur@dhcp-ccc-319.eduroam.cornell.edu>
2019-02-06 15:54:27 -0500

    A function to print zero-order correlation table

---
+++ R/zero_order_corr.r
@@ -0,0 +1,42 @@
+#' Creates a zero-order correlation table
+#' 
+#' Creates a classical zero-order correlation table. A vector with variable names can be added.
+#' 
+#' @param data A data frame containing all variables that should be investigated
+#' @param var_names A vector with fitting variable names.
+#' @param upper_tri Should the upper triangle be omitted?
+#' @examples 
+#' d <- mtcars
+#' 
+#' zero_order_corr(d)
+zero_order_corr <- function(data,
+                            var_names = NULL,
+                            upper_tri = T) {
+  # dependencies
+  library(tidyverse)
+  library(psych)
+  library(magrittr)
+  
+  # function
+  temp <- corr.test(data)$r %>%
+    as.data.frame %>%
+    rownames_to_column("Variables") %>%
+    as.tibble
+  
+  if (!is.null(var_names)) {
+    temp$Variables <- var_names
+  }
+  
+  cols <- c(1:(length(temp)-1)) %>% 
+    as.character
+  temp <- temp %>%
+    set_colnames(c("Variables", cols)) %>%
+    mutate(Variables = paste(cols, Variables))
+  
+  if (isTRUE(upper_tri)) {
+    temp[upper.tri(temp)] <- NA
+  }
+  
+  return(temp)
+  
+}
