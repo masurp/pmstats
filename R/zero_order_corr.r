28a491d13b056fe4e1e50fcf91902d429532e800
Philipp Masur <philippmasur@dhcp-ccc-8916.eduroam.cornell.edu>
2019-02-22 09:56:49 -0500

    Small updates.

--- R/zero_order_corr.r
+++ R/zero_order_corr.r
@@ -1,21 +1,27 @@
 #' Creates a zero-order correlation table
 #' 
-#' Creates a classical zero-order correlation table. A vector with variable names can be added.
+#' This function creates a zero-order correlation table. A vector with variables names can be included in order to replace the first column. Further arguments allow to transform the table into a APA-ready print format.
 #' 
 #' @param data A data frame containing all variables that should be investigated.
 #' @param var_names A vector with fitting variable names.
 #' @param upper_tri Should the upper triangle be omitted?
-#' @param sig Logical vector indicating whether stars should be printed when the effect is significant at alpha = .05.
+#' @param print A logical value indicating whether the table should be formatted according to APA guidelines.
+#' @param digits A number specifying how many digit should be printed.
+#' @param sig Logical value indicating whether stars should be printed when the effect is significant at alpha = .05. Defaults to true when print = TRUE.
 #' @examples 
 #' d <- mtcars
 #' 
+#' # Default
 #' zero_order_corr(d)
-#' zero_order_corr(d, print = T, sig = TRUE)
+#' 
+#' # Customized for printing
+#' zero_order_corr(d, print = T, digits = 3, sig = TRUE)
 #' @export
 zero_order_corr <- function(data,
                             var_names = NULL,
                             upper_tri = TRUE,
                             print = FALSE,
+                            digits = 2,
                             sig = TRUE) {
   # dependencies
   library(tidyverse)
@@ -23,6 +29,7 @@ zero_order_corr <- function(data,
   library(magrittr)
   library(papaja)
   
+  # primary function
   stars <- corr.test(data)$p %>%
     as.data.frame %>%
     rownames_to_column("Variables") %>%
@@ -34,6 +41,7 @@ zero_order_corr <- function(data,
     rownames_to_column("Variables") %>%
     as.tibble
   
+  # customizations based on arguments
   if (!is.null(var_names)) {
     temp$Variables <- var_names
   }
@@ -50,7 +58,8 @@ zero_order_corr <- function(data,
   
   if (isTRUE(print)) {
     temp <- temp %>%
-      mutate_at(vars(2:length(.)), function(x) printnum(x, gt1 = F)) %>%
+      mutate_at(vars(2:length(.)), 
+                function(x) printnum(x, gt1 = F, digits = digits)) %>%
       mutate_at(vars(2:length(.)), 
                 funs(ifelse(. == "NA", "", .)))
   }
@@ -60,13 +69,13 @@ zero_order_corr <- function(data,
   
     for(i in 1:length(temp)) {
       for(j in 1:nrow(temp)){
-      if (isTRUE(stars[i,j])) {
-      temp[i, j] <- paste0(temp[i, j], "*")
-      } else {
-      temp[i, j]
-      }
-    } 
-   }
+        if (isTRUE(stars[i,j])) {
+          temp[i,j] <- paste0(temp[i,j], "*")
+        } else {
+          temp[i,j]
+          }
+      } 
+    }
   }
   
   return(temp)
