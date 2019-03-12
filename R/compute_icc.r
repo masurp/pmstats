ee10527e96cc286a64d7ff39325a946837bfcc19
Philipp Masur <philippmasur@Philipps-MacBook-Pro.local>
2019-03-12 16:19:32 +0100

    Added more digit options to the printing function.

--- R/compute_icc.r
+++ R/compute_icc.r
@@ -4,22 +4,25 @@
 #' 
 #' @param ... One or more objects of class \code{lmer}.
 #' @param var_names By default, the function extracts the name(s) of the dependent variable(s) from the objects. If you provide a vector with new names here, these new names will be used. 
-#' @param digits How many digits should be printed?
+#' @param digits_icc How many digits should be printed for the ICC (only works with \code{print = TRUE})?
+#' @param digits_percent How many digits should be printed for the percentage (only works with \code{print = TRUE})?
 #' @param print Logical value indicating whether the table should be formatted according to APA guidelines. 
 #' @examples 
+#' library(lme4)
 #' # Estimate a multilevel model
 #' mod1 <- lmer(Reaction ~ 1 + (1 | Subject), sleepstudy)
 #' mod2 <- lmer(Reaction ~ 1 + Days + (1 | Subject), sleepstudy)
 #'
 #' # Computing the ICC for the first model
-#' compute_icc(mod1, digits = 3)
+#' compute_icc(mod1, digits_icc = 4, digits_percent = 2, print = TRUE)
 #' 
 #' # Compute ICCs for both models
-#' compute_icc(mod1, mod2, print = T)
+#' compute_icc(mod1, mod2)
 #' @export
 compute_icc <- function(...,
                         var_names = NULL,
-                        digits = 2,
+                        digits_icc = 2,
+                        digits_percent = 1,
                         percent = TRUE,
                         print = FALSE){
   
@@ -72,10 +75,10 @@ compute_icc <- function(...,
                 funs(printnum(., 
                               gt1 = F, 
                               zero = F, 
-                              digits = digits)))
+                              digits = digits_icc)))
     if(isTRUE(percent)) {
       temp <- temp %>%
-        mutate(percent = printnum(percent, digits = 1))
+        mutate(percent = printnum(percent, digits = digits_percent))
     }
   }
   
