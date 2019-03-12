d0c32e8d505b911441be8b0f010c0450949d5e6a
Philipp Masur <philippmasur@Philipps-MacBook-Pro.local>
2019-03-12 16:56:03 +0100

    Updates and links in documentation.

--- R/prior_post_plot.r
+++ R/prior_post_plot.r
@@ -1,10 +1,10 @@
 #' Plotting the prior and posterior distributions of a 
 #' 
-#' This functions takes the output of \code{prior_post_values.r} and creates overlaying density plots for each effect.
+#' This functions takes the output of \code{\link[pmstats]{prior_post_values}} and creates overlaying density plots for each effect.
 #' 
-#' @param object A tibble resulting from the function \code{prior_post_values.r} or any similarly created tibble.
-#' @param extra_key Logical value indicating whether there is more than one key to facet the plots (the second key needs to be named "key2").
-#' @param scales Should the scales be the same of different?
+#' @param object A data frame resulting from the function \code{\link[pmstats]{prior_post_values}} or any similarly created data frame.
+#' @param extra_key Logical value indicating whether there is more than one key to facet the plots (note: if you used the function \code{\link[pmstats]{prior_post_values}} to get the values and did not modify it further, the first key is automatically called "key". A potential second key needs to be named "key2").
+#' @param scales Should scales be fixed ("fixed"), free ("free", the default), or free in one dimension ("free_x", "free_y")?
 #' @return A ggplot object.
 #' @export
 prior_post_plot <- function(object, 
